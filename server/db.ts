f19098478125c64ac21ccf461a5e8cfda0324f50
murtaja <albasti1100@gmail.com>
2024-11-13 12:07:29 +0300

    I add Cluster Url

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,18 @@
+// // server/db.ts
+// import mongoose from 'mongoose';
+
+// const MONGODB_URI = 'mongodb://murtaja:murtaja@ac-qojmvpc-shard-00-00.pd0uhnb.mongodb.net:27017,ac-qojmvpc-shard-00-01.pd0uhnb.mongodb.net:27017,ac-qojmvpc-shard-00-02.pd0uhnb.mongodb.net:27017/?replicaSet=atlas-r8m63a-shard-0&ssl=true&authSource=admin/kwaidi-auth';
+
+// export const connectToDatabase = async () => {
+//   if (mongoose.connection.readyState === 1) return; // Already connected
+//   await mongoose.connect(MONGODB_URI);
+// };
+
+
 // server/db.ts
 import mongoose from 'mongoose';
 
-const MONGODB_URI = 'mongodb://localhost:27017/kwaidi-auth';
+const MONGODB_URI = 'mongodb://murtaja:murtaja@ac-qojmvpc-shard-00-00.pd0uhnb.mongodb.net:27017,ac-qojmvpc-shard-00-01.pd0uhnb.mongodb.net:27017,ac-qojmvpc-shard-00-02.pd0uhnb.mongodb.net:27017/kwaidi-auth?replicaSet=atlas-r8m63a-shard-0&ssl=true&authSource=admin';
 
 export const connectToDatabase = async () => {
   if (mongoose.connection.readyState === 1) return; // Already connected
